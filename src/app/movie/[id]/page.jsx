b96ca777e2509c4051b5fb3b0e89524e26415d90
Farhan <o3190474834@gmail.com>
2024-06-14 08:59:21 +0500

    create header and navbar add dark light mode fun

---
+++ src/app/movie/[id]/page.jsx
@@ -0,0 +1,7 @@
+import Image from 'next/image';
+
+export default async function MoviePage({ params }) {
+  return (
+    <div className='w-full'>MoviePage</div>
+  )
+}
---
+++ src/app/search/[searchTerm]/page.jsx
@@ -0,0 +1,7 @@
+import Results from '@/components/Results';
+
+export default async function SearchPage({ params }) {
+  return (
+    <div>SearchPage</div>
+  )
+}
