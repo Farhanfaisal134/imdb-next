06d7fd150d1ee46cd48f2e7c7af4106b239af5d7
Farhan <o3190474834@gmail.com>
2024-06-14 09:14:09 +0500

    create search box and fetch movie by tmdb api and desplay movie home page

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation'
 
 const SearchBox = () => {
+  const [search, setSearch] = useState('');
+  const router = useRouter();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    router.push(`/search/${search}`);
+  };
   return (
-    <div>SearchBox</div>
+    <form
+      className='flex justify-between px-5 max-w-6xl mx-auto'
+      onSubmit={handleSubmit}
+    >
+      <input
+        type='text'
+        placeholder='Search keywords...'
+        className='w-full h-14 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button
+        className='text-amber-600 disabled:text-gray-400'
+        disabled={search === ''}
+      >
+        Search
+      </button>
+    </form>
   )
 }
 
